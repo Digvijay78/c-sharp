b2ec214213566f27b1913a33ed5c70bc06fb3a33
Digvijay78 <110631494+Digvijay78@users.noreply.github.com>
2024-05-05 01:57:17 +0530

    updates

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   string inp = Console.ReadLine();
+            // Console.WriteLine(inp);
+            //  Console.WriteLine("Hello World");
+
+            //int a = 34;
+            //float b = 34.4F;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a);
+
+            //Console.WriteLine("How many Candies do you want?");
+            //string b = Console.ReadLine();
+
+            //Console.WriteLine("You have 4 more from my side " + (4 + Convert.ToInt32(b)));
+
+            int a = 4;
+            int b = 5;
+            Console.WriteLine("The sum of a and b is :" + (a + b));
+            Console.ReadLine();
+
+        }
+    }
+}
